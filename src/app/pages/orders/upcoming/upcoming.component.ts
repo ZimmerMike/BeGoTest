1223ed3be5d6e8eb64bd8b37190efab3fa637146
Miguel Angel Hernandez Jimenez <miguel.hernandez@netlogistik.local>
2023-04-26 15:04:52 -0600

    Finishing upcoming orders format and design

--- src/app/pages/orders/upcoming/upcoming.component.ts
+++ src/app/pages/orders/upcoming/upcoming.component.ts
@@ -5,10 +5,10 @@ import { QueryService } from '../../../services/query-provider.service';
 @Component({
   selector: 'app-upcoming',
   templateUrl: './upcoming.component.html',
-  styleUrls: ['./upcoming.component.scss']
+  styleUrls: ['./upcoming.component.scss', '../../../app.component.scss']
 })
 export class UpcomingComponent implements OnInit {
-
+  public currentDate: Date;
   public upcomingData: Array<Upcoming>;
   constructor(
     private queryService: QueryService
@@ -16,10 +16,16 @@ export class UpcomingComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.upcomingData = await this.getUpcomingData();
-    console.log(new Date(1654904100000))
+    this.currentDate = this.getDateFormat();
+    console.log(this.currentDate)
+    console.log(this.getDateFormat(1654904100000))
   }
 
   public async getUpcomingData(): Promise<Array<Upcoming>> {
     return await this.queryService.requestUpcoming();
+  }
+
+  public getDateFormat(dateNumber?: number): Date {
+    return dateNumber ? new Date(dateNumber) : new Date();
   }
 }
